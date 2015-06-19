adobe-accessibility/Accessible-Mega-Menu

commit ffe9bb7e37e50f749c24588f2599912ba0979dc1

Don't block clicks on elements inside the toggle
If another element inside the main links used for opening panels
were clicked, the event default would be prevented, and propogation
would be stopped.

This change allows clicks on child elements to be handled as normal.

* Expand the condition in _mouseDownHandler so mouseFocused is set to
  true for elements inside the links, as well as the links themselves.

* Use the currentTarget instead of the target in _clickHandler, so
  that it behaves as if the link was clicked, not a child element

js/jquery-accessibleMegaMenu.js
@@ -285,7 +285,7 @@ limitations under the License.
          * @private
          */
         _clickHandler = function (event) {
-            var target = $(event.target),
+            var target = $(event.currentTarget),
                 topli = target.closest('.' + this.settings.topNavItemClass),
                 panel = target.closest('.' + this.settings.panelClass);
             if (topli.length === 1
@@ -630,7 +630,7 @@ limitations under the License.
          * @private
          */
         _mouseDownHandler = function (event) {
-            if ($(event.target).is(":focusable, ." + this.settings.panelClass)) {
+            if ($(event.target).is(this.settings.panelClass) || $(event.target).closest(":focusable").length) {
                 this.mouseFocused = true;
             }
             this.mouseTimeoutID = setTimeout(function () {

commit b4cd61082b0f4ec404197412944120d5d77eefe3

Allow panels to be opened with the spacebar again
Was broken by ffe9bb7e37e50f749c24588f2599912ba0979dc1,
keep the behaviour of not blocking clicks from nested elements.

js/jquery-accessibleMegaMenu.js
@@ -285,7 +285,7 @@ limitations under the License.
          * @private
          */
         _clickHandler = function (event) {
-            var target = $(event.currentTarget),
+            var target = $(event.target).closest(':tabbable'),
                 topli = target.closest('.' + this.settings.topNavItemClass),
                 panel = target.closest('.' + this.settings.panelClass);
             if (topli.length === 1