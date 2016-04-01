f0038929c6373a29f6b1e13c4fec62adc1be57da
Rich Hauck <rhauck@HII-Macbook-Pro.home>
2016-04-01 01:42:16 -0400

    Made MegaMenu responsive
    - Added demo.html page as an example of responsive nav
    - Added mobileBreakpoint argument to JS to determine when to go mobile
    - Modified JS to pass ES Lint. Removed underscores, modified double
    quotes to single quotes, Fixed Yoda comparisons.

--- js/main.js
+++ js/main.js
@@ -18,11 +18,11 @@ limitations under the License.
 /*global jQuery */
 if (jQuery) {
     (function ($) {
-        "use strict";
+        'use strict';
         $(document).ready(function () {
             // initialize the megamenu
             $('.megamenu').accessibleMegaMenu();
-            
+
             // hack so that the megamenu doesn't show flash of css animation after the page loads.
             setTimeout(function () {
                 $('body').removeClass('init');
