c4eef62bcc5f9c197b3e81889bc8b8b19dbff23a
Maedi Prichard <maedi@morpht.com>
2020-01-24 10:18:46 +1100

    Fix mistargeting of topnav link and panel when no panel.

--- js/jquery-accessibleMegaMenu.js
+++ js/jquery-accessibleMegaMenu.js
@@ -822,8 +822,8 @@ limitations under the License.
                     var topnavitemlink, topnavitempanel;
                     topnavitem = $(topnavitem);
                     topnavitem.addClass(settings.topNavItemClass);
-                    topnavitemlink = topnavitem.find(":tabbable:first");
-                    topnavitempanel = topnavitem.children(":not(:tabbable):last");
+                    topnavitemlink = topnavitem.find("a");
+                    topnavitempanel = topnavitem.find('.' + settings.panelClass);
                     _addUniqueId.call(that, topnavitemlink);
                     if (topnavitempanel.length) {
                         _addUniqueId.call(that, topnavitempanel);
